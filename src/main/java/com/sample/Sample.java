204e866e7dac1d8ea35dd2dceeac1a61fe739c8d
phanikumar1210 <kimandi@miraclesoft.com>
2017-03-16 03:37:29 +0530

    first commit

---
+++ src/main/java/com/sample/Sample.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class Sample {
+	public String Sample()
+	{
+		System.out.println("welcome");
+		return "hai";
+	}
+}
---
+++ src/test/java/com/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		System.out.println("welcome");
+	}
+}
